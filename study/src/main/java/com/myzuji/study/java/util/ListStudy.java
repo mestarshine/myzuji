fce9a73e726a063b7c894b019d84bc46f71a3073
mestarshine <mestarshine@gmail.com>
2020-03-23 07:12:44 +0800

    list 特性用例

--- study/src/main/java/com/myzuji/study/java/util/ListStudy.java
+++ study/src/main/java/com/myzuji/study/java/util/ListStudy.java
@@ -2,7 +2,6 @@ package com.myzuji.study.java.util;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
 /**
  * 说明
@@ -13,7 +12,12 @@ import java.util.List;
 public class ListStudy {
 
     public static void main(String[] args) {
-        List<String> a = new ArrayList(5);
+        arrayListIterator();
+        arrayListToArray();
+    }
+
+    private static void arrayListIterator() {
+        ArrayList<String> a = new ArrayList<>(5);
         a.add("1");
         a.add("2");
         a.add("3");
@@ -22,12 +26,29 @@ public class ListStudy {
         Iterator iterator1 = a.listIterator();
         Iterator iterator2 = a.listIterator(2);
 
-
         while (iterator1.hasNext()) {
-            System.out.println("iterator1:"+iterator1.next());
+            System.out.println("iterator1:" + iterator1.next());
         }
         while (iterator2.hasNext()) {
-            System.out.println("iterator2:"+iterator2.next());
+            System.out.println("iterator2:" + iterator2.next());
         }
     }
+
+    private static void arrayListToArray() {
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        a.add(4);
+        System.out.println("contents of a:" + a);
+        Integer[] ia = new Integer[a.size()];
+        ia = a.toArray(ia);
+        int sum = 0;
+        for (Integer integer : ia) {
+            sum += integer;
+            System.out.println("sum is :"+sum);
+        }
+    }
+
+
 }
