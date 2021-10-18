4fb45490a26e90efb8ecc63a5a91fb7219c3de34
mestarshine <mestarshine@gmail.com>
2021-10-18 20:14:57 +0800

    hadoop wc demo

--- bigdata/src/main/java/com/myzuji/bigdata/hdfs/HDFSTest.java
+++ bigdata/src/main/java/com/myzuji/bigdata/hdfs/HDFSTest.java
@@ -4,51 +4,43 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 
 import java.io.IOException;
 import java.net.URI;
 
 @Slf4j
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class HDFSTest {
 
-    public Configuration configuration = null;
-    public FileSystem fileSystem = null;
+    public static Configuration configuration = null;
+    public static FileSystem fileSystem = null;
 
-    @BeforeEach
-    void conn() throws Exception {
+    public static void main(String[] args) throws Exception {
         configuration = new Configuration(true);
         fileSystem = FileSystem.get(URI.create("hdfs://mycluster"), configuration, "hadoop");
+        // 创建目录
+        mkdir();
+        // 上传文件
+//        put();
 
+        fileSystem.close();
     }
 
-    @Test
-    void mkdir() throws IOException {
+    private static void mkdir() throws IOException {
         Path path = new Path("/data/wc/");
         if (fileSystem.exists(path)) {
             fileSystem.delete(path, true);
         }
 
-//        fileSystem.create(path);
+        fileSystem.create(path);
 
     }
 
-    @Test
-    void put() throws IOException {
+    private static void put() throws IOException {
         Path inputPath = new Path("/tmp/test.txt");
         Path outPath = new Path("/data/wc/input/test.txt");
         if (fileSystem.exists(outPath)) {
             fileSystem.delete(outPath, true);
         }
         fileSystem.completeLocalOutput(outPath, inputPath);
-    }
-
-    @AfterAll
-    void close() throws IOException {
-        fileSystem.close();
     }
 }
--- bigdata/src/main/java/com/myzuji/bigdata/mapreduce/wc/MyWordCount.java
+++ bigdata/src/main/java/com/myzuji/bigdata/mapreduce/wc/MyWordCount.java
@@ -1,6 +1,5 @@
 package com.myzuji.bigdata.mapreduce.wc;
 
-
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -10,31 +9,21 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-
-import java.io.IOException;
 
 @Slf4j
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class MyWordCount {
 
-    public Configuration configuration = null;
-    public FileSystem fileSystem = null;
+    public static Configuration configuration = null;
+    public static FileSystem fileSystem = null;
 
-    @BeforeEach
-    void conn() throws Exception {
+    public static void main(String[] args) throws Exception {
+
         configuration = new Configuration(true);
-
-        configuration.set("mapreduce.admin.user.env", "hadoop");
-        configuration.set("yarn.app.mapreduce.am.admin.user.env", "hadoop");
         // 让框架知道是windows异构平台运行
         configuration.set("mapreduce.app-submission.cross-platform", "true");
 
         Job job = Job.getInstance(configuration);
-//        job.setJar();
+        job.setJar("/Users/starshine/workspace/myzuji/bigdata/build/libs/bigdata-1.0.0-SNAPSHOT.jar");
         job.setJarByClass(MyWordCount.class);
         job.setJobName("Word Count Job Test");
 
@@ -55,13 +44,4 @@ public class MyWordCount {
         job.waitForCompletion(true);
     }
 
-    @Test
-    void task01() {
-
-    }
-
-    @AfterAll
-    void close() throws IOException {
-        fileSystem.close();
-    }
 }
