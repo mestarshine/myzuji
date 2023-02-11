69f1177c08230e3e3fc7f6f45e366bb2db4dedeb
mestarshine <mestarshine@gmail.com>
2023-02-11 10:12:28 +0800

    跟打器修复输入完成后计时器还在走的问题

--- study/typepad/js/typepad.ts
+++ study/typepad/js/typepad.ts
@@ -634,9 +634,7 @@ window.onload = () => {
                     engine.finish();
                 }
             }
-        } else if (engine.isStarted) {
-
-        } else {
+        } else if (!engine.isFinished) {
             engine.start()
         }
     }
