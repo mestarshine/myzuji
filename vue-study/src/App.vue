342b8304ffc33fea726ca381f2c6201e131d89ac
mestarshine <mestarshine@gmail.com>
2023-04-30 10:38:23 +0800

    vue hello world

--- vue-study/src/App.vue
+++ vue-study/src/App.vue
@@ -1,47 +1,7 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from './components/01day/HelloWorld.vue'
 </script>
 
 <template>
-    <header>
-        <img alt="Vue logo" class="logo" height="125" src="./assets/logo.svg" width="125"/>
-
-        <div class="wrapper">
-            <HelloWorld msg="You did it!"/>
-        </div>
-    </header>
-
-    <main>
-        <TheWelcome/>
-    </main>
+    <HelloWorld/>
 </template>
-
-<style scoped>
-header {
-    line-height: 1.5;
-}
-
-.logo {
-    display: block;
-    margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-    header {
-        display: flex;
-        place-items: center;
-        padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-        margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-        display: flex;
-        place-items: flex-start;
-        flex-wrap: wrap;
-    }
-}
-</style>
