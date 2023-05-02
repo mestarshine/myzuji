942c958f71a2ca2ca7f8250b4075232d41cc59d7
mestarshine <mestarshine@gmail.com>
2023-05-02 08:40:37 +0800

    原始 HTML

--- vue-study/src/components/01day/TemplateSyntax.vue
+++ vue-study/src/components/01day/TemplateSyntax.vue
@@ -1,7 +1,10 @@
 <script setup>
 const message = 'Hello World!'
+const rawHtml = '<span style="color: red">This should be red.</span>'
 </script>
 
 <template>
     <h1>{{ message }}</h1>
+    <p>Using text interpolation: {{ rawHtml }}</p>
+    <p>Using v-html directive: <span v-html="rawHtml"></span></p>
 </template>
