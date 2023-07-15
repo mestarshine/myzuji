6bdeefe72483b2dab77b5f7ad1e09758e9e6514c
mestarshine <mestarshine@gmail.com>
2023-07-16 06:34:23 +0800

    reactive 创建对像类型的响应式数据

--- vue-study/src/components/Person.vue
+++ vue-study/src/components/Person.vue
@@ -1,32 +1,30 @@
 <script lang="ts" setup name="Person5">
-import {ref} from "vue";
-//数据
-let userName = ref("张三"); // 不是响应式的
-let age = ref(18);
-let tel = '133338888';
+import {reactive} from "vue";
+// 数据
+let car = reactive({brand: 'BMW', price: 100})
+let moves = reactive([
+    {id:'abc1',name:'阿Q正传'},
+    {id:'abc2',name:'肖恩克的救赎'}
+])
 
-//方法
-function changeName() {
-    userName.value = "李四";
+function changePrice() {
+    car.price += 10
 }
 
-function changeAge() {
-    age.value += 1;
+function addMove() {
+    moves.push({id:'abc3',name:'笑傲江湖'})
 }
-
-function showTel() {
-    alert(tel);
-}
-
 </script>
 
 <template>
     <div>
-        <h2>姓名：{{ userName }}</h2>
-        <h2>年龄：{{ age }}</h2>
-        <button @click="changeName">修改名字</button>
-        <button @click="changeAge">修改年龄</button>
-        <button @click="showTel">查看联系方式</button>
+        <h1>一辆{{ car.brand }}车，价值{{ car.price }}</h1>
+        <button @click="changePrice">修改汽车价格</button>
+        <h1>电影列表</h1>
+        <button @click="addMove">添加一部</button>
+        <ul>
+            <li v-for="m in moves" :key="m.id">{{m.name}}</li>
+        </ul>
     </div>
 </template>
 
