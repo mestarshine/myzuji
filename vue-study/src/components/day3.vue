579397ff55569e879977c3daaf2bf52ada31691b
mestarshine <mestarshine@gmail.com>
2023-11-18 16:07:37 +0800

    watch ref 定义的对象类型数据

--- vue-study/src/components/day3.vue
+++ vue-study/src/components/day3.vue
@@ -25,12 +25,44 @@ const stopWatch = watch(sum,(newValue,oldValue)=>{
         stopWatch();
     }
 })
+
+// ref 定义对象类型的数据
+let person = ref({
+    name:'张三',
+    age:18
+})
+
+function changeName() {
+    person.value.name += "*";
+}
+function changeAge() {
+    person.value.age += 1
+}
+
+function changePerson() {
+    person.value={name: '李四', age: 40}
+}
+
+/*
+    监视【ref】定义的【对象类型】，监视的是对象的地址值
+    若想监视对象内部属性的变化，需要手动开启深度监视
+ */
+watch(person, (newValue, oldValue) => {
+    console.log("person 变化了",oldValue,newValue)
+},{deep:true});
 </script>
 <template>
     <div class="day3">
         <h1>情况一：监视【ref】定义的【基本类型】数据</h1>
         <h2>当前求和为：{{sum}}</h2>
         <button @click="changeSum">加一</button>
+
+        <h1>情况二：监视【ref】定义的【对象类型】数据</h1>
+        <h2>姓名：{{person.name}}</h2>
+        <h2>年龄：{{person.age}}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="changePerson">换个人 </button>
     </div>
 </template>
 
