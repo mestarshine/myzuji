96529dc56b04cb86b12e39254a27e245c6cbe55b
mestarshine <mestarshine@gmail.com>
2023-12-03 14:44:38 +0800

    标签的ref属性

--- vue-study/src/App.vue
+++ vue-study/src/App.vue
@@ -1,17 +1,12 @@
+<script lang="ts" setup name="App">
+// JS 或 TS
+import day from './components/day5.vue';
+
+</script>
+
 <template>
     <day/>
 </template>
-
-<script lang="ts" name="App">
-// JS 或 TS
-import day from "@/components/day4.vue";
-
-export default {
-    components:{
-        day
-    }
-}
-</script>
 
 <style>
 /* 样式 */
---
+++ vue-study/src/components/day5.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts" name="day5">
+
+import {ref} from "vue";
+
+/*
+    标签的 【ref】属性
+    用于普通【DOM】标签上，获取的是【DOM】节点
+    用于组件标签上，获取的是组件实例对象
+ */
+let test = ref();
+
+function showLog(){
+    console.log(test);
+}
+</script>
+
+<template>
+    <div class="day5">
+        <h2 ref="test">测试Ref</h2>
+        <button @click="showLog">打印内容</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
