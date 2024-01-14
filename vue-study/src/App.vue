0d4aba66ed5936870c0b43fe9672b9ad9e08eac5
mestarshine <mestarshine@gmail.com>
2024-01-14 08:50:29 +0800

    路由器 to 的两种使用方式

--- vue-study/src/App.vue
+++ vue-study/src/App.vue
@@ -9,7 +9,7 @@
         <div class="navigate">
             <RouterLink to="/home" active-class="active">首页</RouterLink>
             <RouterLink to="/news" active-class="active">新闻</RouterLink>
-            <RouterLink to="/about" active-class="active">关于</RouterLink>
+            <RouterLink :to="{path:'/about'}" active-class="active">关于</RouterLink>
         </div>
         <!-- 展示区 -->
         <div class="main-content">
@@ -21,19 +21,22 @@
 
 <style scoped>
 /* 样式 */
-.app{
+.app {
     text-align: center;
     height: 200px; /* 示例高度 */
 }
-.navigate{
+
+.navigate {
     display: block;
     height: 30px;
     margin: 50px 100px 10px;
 }
-.navigate a{
+
+.navigate a {
     margin: 50px 100px 10px;
 }
-.main-content{
+
+.main-content {
     border: #222222 solid 1px;
     height: 260px;
 }
