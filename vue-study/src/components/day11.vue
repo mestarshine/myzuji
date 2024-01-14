999c2d04b912d2b4239c5ef7a0c93e6c6bc1939a
mestarshine <mestarshine@gmail.com>
2024-01-14 08:29:43 +0800

    路由组件与一般组件的简单使用

---
+++ vue-study/src/components/day11.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="day11">
+        路由组件：靠路由的规则渲染出来的，pages/views中<br/>
+        一般组件：亲手写标签出来的，放在components中<br/>
+        视觉了“消失”了的路由组件，默认是被“卸载”了，需要的时候再去”挂载“
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- vue-study/src/router/index.ts
+++ vue-study/src/router/index.ts
@@ -1,15 +1,16 @@
 import {createRouter, createWebHistory} from "vue-router";
 // 引入要呈现的组件
-import day10 from "@/components/day10.vue"
-import day10_1 from "@/components/day10_1.vue"
-import day10_2 from "@/components/day10_2.vue"
+import day11 from "@/components/day11.vue"
+import day10 from "@/views/day10.vue"
+import day10_1 from "@/views/day10_1.vue"
+import day10_2 from "@/views/day10_2.vue"
 
 // 创建路由器
 let router = createRouter({
     history: createWebHistory(), //路由器的工作模式
     routes: [{   //一个一个的路由规则
         path: '/',
-        component: day10
+        component: day11
     },{   //一个一个的路由规则
         path: '/home',
         component: day10
