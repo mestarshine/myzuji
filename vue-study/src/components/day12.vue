982c5c84ad4bc430510445af9f83bb318ecea10a
mestarshine <mestarshine@gmail.com>
2024-01-14 08:46:06 +0800

    路由器工作模式

---
+++ vue-study/src/components/day12.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="day11">
+        <h2>路由器工作模式</h2>
+        <p>1. history 模式</p>>
+        <p>vue2 : mode:'history'</p>
+        <p>vue3 : history:createWebHistory()</p>
+        <p>
+            优点：URL 更加美观，不带有#，更接近传统的网站URL
+            缺点：后期项目上线，需要服务端配合处理路径问题，否则刷新会有404错误
+        </p>
+        <br/>
+        <p>2. hash 模式</p>
+        <p>vue3 : history:createHashHistory()</p>
+        <p>
+            优点：兼容性好，因为不需要服务器端处理路径
+            缺点：URL 带有 # 不太美观，且在 SEO 优化方面相对较差
+        </p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- vue-study/src/router/index.ts
+++ vue-study/src/router/index.ts
@@ -1,6 +1,6 @@
 import {createRouter, createWebHistory} from "vue-router";
 // 引入要呈现的组件
-import day11 from "@/components/day11.vue"
+import day12 from "@/components/day12.vue"
 import day10 from "@/views/day10.vue"
 import day10_1 from "@/views/day10_1.vue"
 import day10_2 from "@/views/day10_2.vue"
@@ -10,7 +10,7 @@ let router = createRouter({
     history: createWebHistory(), //路由器的工作模式
     routes: [{   //一个一个的路由规则
         path: '/',
-        component: day11
+        component: day12
     },{   //一个一个的路由规则
         path: '/home',
         component: day10
