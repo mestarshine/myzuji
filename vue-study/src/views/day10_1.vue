498bcc63349d92d47cc1b93c8f82892cdc062393
mestarshine <mestarshine@gmail.com>
2024-01-21 10:10:09 +0800

    路由query传参方式第二种写法

--- vue-study/src/views/day10_1.vue
+++ vue-study/src/views/day10_1.vue
@@ -2,11 +2,11 @@
 
 import {reactive} from "vue";
 
-const  newsList = reactive([
-    {id:'dfafa01',title:'新闻一',content:'新闻一新闻一新闻一新闻一'},
-    {id:'dfafa02',title:'新闻二',content:'新闻二新闻二新闻二新闻二'},
-    {id:'dfafa03',title:'新闻三',content:'新闻三新闻三新闻三新闻三'},
-    {id:'dfafa04',title:'新闻四',content:'新闻四新闻四新闻四新闻四'}
+const newsList = reactive([
+    {id: 'dfafa01', title: '新闻一', content: '新闻一新闻一新闻一新闻一'},
+    {id: 'dfafa02', title: '新闻二', content: '新闻二新闻二新闻二新闻二'},
+    {id: 'dfafa03', title: '新闻三', content: '新闻三新闻三新闻三新闻三'},
+    {id: 'dfafa04', title: '新闻四', content: '新闻四新闻四新闻四新闻四'}
 ])
 </script>
 
@@ -14,7 +14,21 @@ const  newsList = reactive([
     <div>
         <ul class="news">
             <li v-for="news in newsList" :key="news.id">
-                <RouterLink :to="`/news/detail?id=${news.id}&title=${news.title}&content=${news.content}`">{{news.title}}</RouterLink>
+                <!-- 第一种写法 -->
+                <RouterLink :to="`/news/detail?id=${news.id}&title=${news.title}&content=${news.content}`">
+                    {{ news.title }}
+                </RouterLink>
+                <!-- 第二种写法 -->
+                <RouterLink :to="{
+                    path: '/news/detail',
+                    query:{
+                        id: news.id,
+                        title: news.title,
+                        content:news.content
+                    }
+                }">
+                    {{ news.title }}
+                </RouterLink>
             </li>
         </ul>
         <div class="news-content">
@@ -30,6 +44,7 @@ const  newsList = reactive([
     list-style-type: none;
     padding: 0;
 }
+
 .news-content {
     float: left;
     width: 89%;
