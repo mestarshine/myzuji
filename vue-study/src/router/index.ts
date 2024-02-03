ff4d4760bf70161e2971a19a01eb3839a5e80bf0
mestarshine <mestarshine@gmail.com>
2024-02-03 11:01:06 +0800

    路由 props 传参第三种写法及路由组件的调整

--- vue-study/src/router/index.ts
+++ vue-study/src/router/index.ts
@@ -1,10 +1,10 @@
 import {createRouter, createWebHistory} from "vue-router";
 // 引入要呈现的组件
-import day12 from "@/components/day12.vue"
+import day12 from "@/views/day12.vue"
 import day10 from "@/views/day10.vue"
-import day10_1 from "@/views/day10_1.vue"
 import day10_2 from "@/views/day10_2.vue"
-import day14 from "@/components/day14.vue";
+import day15_1 from "@/views/day15_1.vue";
+import day15 from "@/views/day15.vue";
 
 // 创建路由器
 let router = createRouter({
@@ -18,18 +18,24 @@ let router = createRouter({
     },{   //一个一个的路由规则
         name: 'xinwen',
         path: '/news',
-        component: day10_1,
+        component: day15_1,
         children:[
             {
                 name: 'xiangqing',
                 path: 'detail/:id/:title/:content?',
-                component: day14,
+                component: day15,
                 // 第一种写法：将路由收到的所有 params 参数作为 props 传给路由组件
-                // props:true
-                // 第二种写法：可以自己决定将什么作为 props 给路由组件
-                props(route) {
-                    return route.params
-                },
+                props:true
+                // 第二种写法：函数写法，可以自己决定将什么作为 props 给路由组件
+                // props(route) {
+                //     return route.params
+                // },
+                // 第三种写法：对象写法，可以自己决定将什么作为 props 给路由组件
+                // props: {
+                //     id:'xx',
+                //     title:'xx',
+                //     content:'xx',
+                // },
             }
         ]
     },{   //一个一个的路由规则
