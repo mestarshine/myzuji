d102bd57d84bd0a0955aef21f945f6a391bf558c
mestarshine <mestarshine@gmail.com>
2024-03-02 17:42:56 +0800

    修改 pinia 中存储的数据的第三种方法 actions

--- vue-study/src/components/day16_1.vue
+++ vue-study/src/components/day16_1.vue
@@ -1,28 +1,24 @@
 <script setup lang="ts" name="day16_1">
-import axios from "axios";
-import {nanoid} from "nanoid";
 import {userTalkStore} from "@/store/day16_1";
 
 let talkStore = userTalkStore();
 
-async function getLoveTalk() {
-    let {data:{content:title}} = await axios.get('https://api.uomg.com/api/rand.qinghua?format=json');
-    let obj = {id: nanoid(), title}
-    talkStore.talkList.unshift(obj)
+function getLoveTalk() {
+    talkStore.getATalk();
 }
 </script>
 
 <template>
-<div class="talk">
-<button @click="getLoveTalk">获取一句土味情话</button>
-    <ul>
-        <li v-for="talk in talkStore.talkList" :key="talk.id">{{talk.title}}</li>
-    </ul>
-</div>
+    <div class="talk">
+        <button @click="getLoveTalk">获取一句土味情话</button>
+        <ul>
+            <li v-for="talk in talkStore.talkList" :key="talk.id">{{ talk.title }}</li>
+        </ul>
+    </div>
 </template>
 
 <style scoped>
-.talk{
+.talk {
     background-color: lightgreen;
     padding: 10px;
     border-radius: 10px;
--- vue-study/src/store/day16_1.ts
+++ vue-study/src/store/day16_1.ts
@@ -1,6 +1,15 @@
 import {defineStore} from "pinia";
+import axios from "axios";
+import {nanoid} from "nanoid";
 
 export let userTalkStore = defineStore('talk', {
+    actions:{
+      async getATalk(){
+          let {data:{content:title}} = await axios.get('https://api.uomg.com/api/rand.qinghua?format=json');
+          let obj = {id: nanoid(), title}
+          this.talkList.unshift(obj)
+      }
+    },
     // 真正 存储数据的地方
     state() {
         return {
