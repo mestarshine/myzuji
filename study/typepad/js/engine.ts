87a30961b9b50be23998afa6e0ce88a8fae5e04b
mestarshine <mestarshine@gmail.com>
2025-01-26 15:33:25 +0800

    跟打器优化引擎初始化

--- study/typepad/js/engine.ts
+++ study/typepad/js/engine.ts
@@ -1,6 +1,9 @@
-// 跟打器引擎
-import {KeyCount} from "./keyCount";
-import {ARTICLE, currentOriginWords} from "./constants";
+import { KeyCount } from './keyCount';
+import { Records } from './records';
+import { Config } from './config';
+import { shuffle } from './utils';
+import {ARTICLE, HEIGHT_TEMPLATE, UNTYPED_STRING_CLASSNAME} from './constants'
+import {DataBase} from "./dataBase";
 
 export class Engine {
     isFinished: boolean = false;
@@ -11,6 +14,31 @@ export class Engine {
     duration: number = 0; // ms
     handleRefresh: number | undefined;
     refreshRate: number = 500; // ms
+    currentWords:string="";
+    currentOriginWords:string[] = [];
+    correctWordsCount:number = 0;
+
+    constructor(private config: Config, private count: KeyCount, private record: Records,private dataBase:DataBase) {}
+
+    init(content:HTMLElement|any):void{
+        this.loadArticles();
+        // 最开始的时候，如果没有检测到存储的数据，初始化
+        if (this.config.hasSavedData()) {
+            this.config.get();
+            this.config.setWithCurrentConfig();
+            this.currentOriginWords = this.config.article.split('');
+        } else {
+            this.config.save();
+            this.config.get();
+            this.config.setWithCurrentConfig();``
+            this.currentOriginWords = this.config.article.split('');
+            this.changePerCount();
+        }
+
+        this.currentWords = this.currentOriginWords.slice(this.config.count * (this.config.chapter - 1), this.config.count * this.config.chapter).join('');
+        if (content) content.innerText = this.currentWords;
+        this.updateCountInfo();
+    }
 
     start(): void {
         this.isStarted = true;
@@ -58,7 +86,6 @@ export class Engine {
         }
     }
 
-    // 暂停
     pause(): void {
         this.isPaused = true;
         const typingPad = document.querySelector('#pad') as HTMLTextAreaElement;
@@ -66,23 +93,21 @@ export class Engine {
         this.stopRefresh();
     }
 
-    // 继续
     resume(): void {
         this.timeStart = (new Date()).getTime() - this.duration;
         this.isPaused = false;
         this.startRefresh();
     }
 
-    // 重置计数器
     reset(): void {
-        record = new Records();
+        this.record = new Records();
         const content = document.querySelector('.content p') as HTMLElement;
-        if (content) content.innerHTML = currentWords;
+        if (content) content.innerHTML = this.currentWords;
 
         const typingPad = document.querySelector('#pad') as HTMLTextAreaElement;
         if (typingPad) typingPad.value = '';
 
-        count.reset();
+        this.count.reset();
         this.updateCountInfo();
         this.isPaused = false;
         this.isStarted = false;
@@ -91,21 +116,20 @@ export class Engine {
         this.showTime();
     }
 
-    // 乱序当前段
     shuffle(): void {
-        const array = currentWords.split('');
-        currentWords = shuffle(array).join('');
+        const array = this.currentWords.split('');
+        this.currentWords = shuffle(array).join('');
         const content = document.querySelector('.content p') as HTMLElement;
-        if (content) content.innerText = currentWords;
+        if (content) content.innerText = this.currentWords;
         this.isFinished = false;
         this.reset();
     }
 
     compare(): void {
-        correctWordsCount = 0;
+        this.correctWordsCount = 0;
         const typingPad = document.querySelector('#pad') as HTMLTextAreaElement;
         const typedWords = typingPad ? typingPad.value : '';
-        const arrayOrigin = currentWords.split('');
+        const arrayOrigin = this.currentWords.split('');
         const arrayTyped = typedWords.split('');
         let html = '';
         let lastCharacterIsCorrect = false; // 上一个字符是正确的
@@ -116,7 +140,7 @@ export class Engine {
             const origin = arrayOrigin[index] || ' ';
             const currentCharacterIsCorrect = current === origin;
             if (currentCharacterIsCorrect) {
-                correctWordsCount++;
+                this.correctWordsCount++;
                 wordsCorrect = wordsCorrect.concat(origin);
             } else {
                 wordsWrong = wordsWrong.concat(origin);
@@ -139,14 +163,14 @@ export class Engine {
             lastCharacterIsCorrect = current === origin;
         });
 
-        const untypedString = currentWords.substring(arrayTyped.length);
-        const untypedHtml = `<span class='${untypedStringClassName}'>${untypedString}</span>`;
+        const untypedString = this.currentWords.substring(arrayTyped.length);
+        const untypedHtml = `<span class='${UNTYPED_STRING_CLASSNAME}'>${untypedString}</span>`;
         html = html.concat(untypedHtml);
         const content = document.querySelector('.content p') as HTMLElement;
         if (content) content.innerHTML = html;
 
         // 滚动对照区到当前所输入的位置
-        const untypedElement = document.querySelector(`.${untypedStringClassName}`) as HTMLElement;
+        const untypedElement = document.querySelector(`.${UNTYPED_STRING_CLASSNAME}`) as HTMLElement;
         const contentWrapper = document.querySelector('.content') as HTMLElement;
         if (untypedElement && contentWrapper) {
             const offsetTop = untypedElement.offsetTop;
@@ -154,7 +178,6 @@ export class Engine {
         }
     }
 
-    // 当前段打完
     finish(): void {
         this.isStarted = false;
         this.isFinished = true;
@@ -163,20 +186,19 @@ export class Engine {
         this.duration = this.timeEnd - this.timeStart;
 
         // update record
-        record.backspace = count.backspace;
-        record.timeStart = this.timeStart;
-        record.duration = this.duration;
-        record.wordCount = currentWords.length;
-        record.articleName = config.articleName;
+        this.record.backspace = this.count.backspace;
+        this.record.timeStart = this.timeStart;
+        this.record.duration = this.duration;
+        this.record.wordCount = this.currentWords.length;
+        this.record.articleName = this.config.articleName;
         this.updateCountInfo();
-        dataBase.insert(record);
+        this.dataBase.insert(this.record);
 
-        if (config.isAutoNext) {
+        if (this.config.isAutoNext) {
             this.nextChapter();
         }
     }
 
-    // 更新界面信息
     updateCountInfo(): void {
         const timeElement = document.querySelector('.time') as HTMLElement;
         if (timeElement) {
@@ -188,13 +210,13 @@ export class Engine {
         }
 
         // KEY COUNT
-        for (const type in count) {
+        for (const type in this.count) {
             const element = document.querySelector(`.word-${type} p`) as HTMLElement;
-            if (element) element.innerText = count[type as keyof KeyCount].toString();
+            if (element) element.innerText = this.count[type as keyof KeyCount].toString();
         }
 
         const countTotalElement = document.querySelector('.count-total') as HTMLElement;
-        if (countTotalElement) countTotalElement.innerText = currentWords.length.toString();
+        if (countTotalElement) countTotalElement.innerText = this.currentWords.length.toString();
 
         const countCurrentElement = document.querySelector('.count-current') as HTMLElement;
         const typingPad = document.querySelector('#pad') as HTMLTextAreaElement;
@@ -214,31 +236,31 @@ export class Engine {
             if (countKeyLengthElement) countKeyLengthElement.innerText = '--';
             if (countKeyBackspaceElement) countKeyBackspaceElement.innerText = '--';
         } else {
-            record.speed = (correctWordsCount / this.duration * 1000 * 60).toFixed(2);
-            if (speedElement) speedElement.innerText = record.speed;
-            if (btnSpeedElement) btnSpeedElement.innerText = record.speed;
+            this.record.speed = (this.correctWordsCount / this.duration * 1000 * 60).toFixed(2);
+            if (speedElement) speedElement.innerText = this.record.speed;
+            if (btnSpeedElement) btnSpeedElement.innerText = this.record.speed;
 
-            const keyCount = count.all - count.function;
-            record.hitRate = (keyCount / this.duration * 1000).toFixed(2);
-            if (countKeyRateElement) countKeyRateElement.innerText = record.hitRate;
+            const keyCount = this.count.all - this.count.function;
+            this.record.hitRate = (keyCount / this.duration * 1000).toFixed(2);
+            if (countKeyRateElement) countKeyRateElement.innerText = this.record.hitRate;
 
             // code length
-            if (correctWordsCount) {
-                record.codeLength = (keyCount / correctWordsCount).toFixed(2);
+            if (this.correctWordsCount) {
+                this.record.codeLength = (keyCount / this.correctWordsCount).toFixed(2);
             } else {
-                record.codeLength = '0';
+                this.record.codeLength = '0';
             }
-            if (countKeyLengthElement) countKeyLengthElement.innerText = record.codeLength;
+            if (countKeyLengthElement) countKeyLengthElement.innerText = this.record.codeLength;
 
             // backspace count
-            if (countKeyBackspaceElement) countKeyBackspaceElement.innerText = count.backspace.toString();
+            if (countKeyBackspaceElement) countKeyBackspaceElement.innerText = this.count.backspace.toString();
         }
 
         const chapterCurrentElement = document.querySelector('.chapter-current') as HTMLElement;
-        if (chapterCurrentElement) chapterCurrentElement.innerText = config.chapter.toString();
+        if (chapterCurrentElement) chapterCurrentElement.innerText = this.config.chapter.toString();
 
         const chapterTotalElement = document.querySelector('.chapter-total') as HTMLElement;
-        if (chapterTotalElement) chapterTotalElement.innerText = config.chapterTotal.toString();
+        if (chapterTotalElement) chapterTotalElement.innerText = this.config.chapterTotal.toString();
     }
 
     changeArticle(): void {
@@ -249,77 +271,83 @@ export class Engine {
         const article = ARTICLE[articleNameValue];
         if (!article) return;
 
-        config.articleName = article.name;
-        config.articleNameValue = articleNameValue;
-        const content = ARTICLE[config.articleNameValue].content;
-        currentOriginWords = config.isShuffle ? shuffle(content.split('')) : content.split('');
-        config.article = currentOriginWords.join('');
+        this.config.articleName = article.name;
+        this.config.articleNameValue = articleNameValue;
+        const content = ARTICLE[this.config.articleNameValue].content;
+        this.currentOriginWords = this.config.isShuffle ? shuffle(content.split('')) : content.split('');
+        this.config.article = this.currentOriginWords.join('');
         this.changePerCount();
     }
 
-    // 改变数字时
     changePerCount(): void {
         const countRadio = document.querySelector('input[type=radio]:checked') as HTMLInputElement;
         if (!countRadio) return;
 
-        config.count = Number(countRadio.value);
-        if (config.count === Number.MAX_SAFE_INTEGER) {
-            currentWords = currentOriginWords.join('');
+        this.config.count = Number(countRadio.value);
+        if (this.config.count === Number.MAX_SAFE_INTEGER) {
+            this.currentWords = this.currentOriginWords.join('');
         } else {
-            currentWords = currentOriginWords.slice(0, config.count).join('');
+            this.currentWords = this.currentOriginWords.slice(0, this.config.count).join('');
         }
-        config.chapter = 1;
-        const originTol = currentOriginWords.length / config.count;
+        this.config.chapter = 1;
+        const originTol = this.currentOriginWords.length / this.config.count;
         const tempTol = Math.floor(originTol);
-        if (config.count === Number.MAX_SAFE_INTEGER) {
-            config.chapterTotal = 1;
+        if (this.config.count === Number.MAX_SAFE_INTEGER) {
+            this.config.chapterTotal = 1;
         } else {
-            config.chapterTotal = originTol > tempTol ? tempTol + 1 : tempTol;
+            this.config.chapterTotal = originTol > tempTol ? tempTol + 1 : tempTol;
         }
-        config.save();
+        this.config.save();
         this.reset();
     }
 
-    // 自动发文
     autoNext(): void {
         const autoNextCheckbox = document.querySelector('#autoNext') as HTMLInputElement;
         if (!autoNextCheckbox) return;
 
-        config.isAutoNext = autoNextCheckbox.checked;
-        config.save();
+        this.config.isAutoNext = autoNextCheckbox.checked;
+        this.config.save();
     }
 
-    // 切换乱序模式
     shuffleCurrentArticle(): void {
         const shuffleModeCheckbox = document.querySelector('#shuffleMode') as HTMLInputElement;
         if (!shuffleModeCheckbox) return;
 
-        config.isShuffle = shuffleModeCheckbox.checked;
-        currentOriginWords = config.isShuffle ? shuffle(ARTICLE[config.articleNameValue].content.split('')) : ARTICLE[config.articleNameValue].content.split('');
-        config.article = currentOriginWords.join('');
-        currentWords = currentOriginWords.slice(0, config.count).join('');
-        config.chapter = 1;
-        config.save(); // save config
+        this.config.isShuffle = shuffleModeCheckbox.checked;
+        this.currentOriginWords = this.config.isShuffle ? shuffle(ARTICLE[this.config.articleNameValue].content.split('')) : ARTICLE[this.config.articleNameValue].content.split('');
+        this.config.article = this.currentOriginWords.join('');
+        this.currentWords = this.currentOriginWords.slice(0, this.config.count).join('');
+        this.config.chapter = 1;
+        this.config.save(); // save config
         this.reset();
     }
 
-    // 上一段
     prevChapter(): void {
-        if (config.chapter !== 1) {
-            currentWords = currentOriginWords.slice(config.count * (config.chapter - 2), config.count * (config.chapter - 1)).join('');
-            config.chapter--;
+        if (this.config.chapter !== 1) {
+            this.currentWords = this.currentOriginWords.slice(this.config.count * (this.config.chapter - 2), this.config.count * (this.config.chapter - 1)).join('');
+            this.config.chapter--;
             this.reset();
-            config.save();
+            this.config.save();
         }
     }
 
-    // 下一段
     nextChapter(): void {
-        if (config.chapter !== config.chapterTotal) {
-            currentWords = currentOriginWords.slice(config.count * config.chapter, config.count * (config.chapter + 1)).join('');
-            config.chapter++;
+        if (this.config.chapter !== this.config.chapterTotal) {
+            this.currentWords = this.currentOriginWords.slice(this.config.count * this.config.chapter, this.config.count * (this.config.chapter + 1)).join('');
+            this.config.chapter++;
             this.reset();
-            config.save();
+            this.config.save();
         }
+    }
+
+    loadArticles(): void {
+        let optionHtml = '';
+        for (const itemName in ARTICLE) {
+            const article = ARTICLE[itemName];
+            const tempHtml = `<option value="${itemName}">${article.name}</option>`;
+            optionHtml += tempHtml;
+        }
+        const articleSelect = document.querySelector('#article') as HTMLSelectElement;
+        if (articleSelect) articleSelect.innerHTML = optionHtml;
     }
 }
